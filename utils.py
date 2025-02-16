f9a3d44071f074a7a778fd668377a85ad7b9452d
Evgenii Ptitsyn <eptitsyn@gmail.com>
2025-02-17 01:24:04 +0300

    utils

---
+++ utils.py
@@ -0,0 +1,36 @@
+import math
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LambdaLR
+
+def get_cosine_schedule_with_warmup(
+    optimizer: Optimizer,
+    num_warmup_steps: int,
+    num_training_steps: int,
+    num_cycles: float = 0.5,
+    last_epoch: int = -1
+):
+    """
+    Create a schedule with a learning rate that decreases following the values of the cosine function between the
+    initial lr set in the optimizer to 0, after a warmup period during which it increases linearly between 0 and the
+    initial lr set in the optimizer.
+
+    Args:
+        optimizer (Optimizer): The optimizer for which to schedule the learning rate
+        num_warmup_steps (int): The number of steps for the warmup phase
+        num_training_steps (int): The total number of training steps
+        num_cycles (float): The number of cosine cycles. Default: 0.5
+        last_epoch (int): The index of the last epoch when resuming training. Default: -1
+
+    Return:
+        `torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
+    """
+    def lr_lambda(current_step):
+        # Warmup
+        if current_step < num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+
+        # Cosine decay
+        progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
+        return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
+
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
